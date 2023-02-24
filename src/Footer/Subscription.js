45b6c73bfdaa7276bbb230116f0159238435b502
AnnaMATI <matinyan-1998@mail.ru>
2023-02-24 14:23:33 +0400

    commit

--- src/Footer/Subscription.js
+++ src/Footer/Subscription.js
@@ -1,25 +1,73 @@
-const  icon=[`https://cdn-icons-png.flaticon.com/512/2111/2111463.png`,
-`https://cdn-icons-png.flaticon.com/512/2504/2504903.png`,
-`https://cdn-icons-png.flaticon.com/512/3536/3536424.png`
-]
+        // const  icon=[`https://cdn-icons-png.flaticon.com/512/2111/2111463.png`,
+        // `https://cdn-icons-png.flaticon.com/512/2504/2504903.png`,
+        // `https://cdn-icons-png.flaticon.com/512/3536/3536424.png`
+        // ]
 
-export default function Sub(){
-return(
-    <footer className="foot">
-            <div className="sub1">
-                     <h3>SUBSCRIBE</h3>
-                    <label> Your first name: <input name="firstName" /></label> 
-                    <label> Your e-mail address: <input name="email" /> </label> 
-                    <button type="submit">Submit form</button>
-            </div>
-           < div className="icon">
-            <img src={icon[0]}/>
-            <img src={icon[1]}/>
-            <img src={icon[2]}/>
-        </div>    
-</footer>
+        export default function Sub(){
+        return(
+        <footer className="foot">     
+        <div className="cols">     
+                <div className="sub1">
+                        <h3>SUBSCRIBE</h3>
+                        <label> Your first name: <input name="firstName" /></label> 
+                        <label> Your e-mail address: <input name="email" /> </label> 
+                        <button type="submit">Submit form</button>   
+                </div>
+                        <div className="col">
+                                        <h2>Company</h2>
+                                        <ul>
+                                                <li>About</li>
+                                                <li>Mission</li>
+                                                <li>Services</li>
+                                                <li>Social</li>
+                                                <li>Get in touch</li>
+                                        </ul>
+                                        </div>  
+                        <div className="col">
+                                <h2>Products</h2>
+                                <ul>
+                                        <li>About</li>
+                                        <li>Mission</li>
+                                        <li>Services</li>
+                                        <li>Social</li>
+                                        <li>Get in touch</li>
+                                </ul>
+                        </div>  
+                        <div className="col">
+                                <h2>Accounts</h2>
+                                <ul>
+                                        <li>About</li>
+                                        <li>Mission</li>
+                                        <li>Services</li>
+                                        <li>Social</li>
+                                        <li>Get in touch</li>
+                                </ul>
+                                        </div>
+                                        <div className="col">
+                                                <h2>Support</h2>
+                                                <ul>
+                                                        <li>Contact us</li>
+                                                        <li>Web chat</li>
+                                                        <li>Open ticket</li>
+                                                </ul>
+                                        </div>
+                                                                        </div> 
 
-)}
+        <div class="col social">
+                <h2>Social</h2>
+        <ul>
+                <li> <img src="https://cdn-icons-png.flaticon.com/512/2111/2111463.png"   />  </li>
+                <li><img src="https://cdn-icons-png.flaticon.com/512/2504/2504903.png"   /> </li>
+                <li><img src="https://cdn-icons-png.flaticon.com/512/3536/3536424.png"   /> </li>
+        </ul>
+                </div>
+
+        </footer>
+
+        )
+        }
 
 
 
+
+   
